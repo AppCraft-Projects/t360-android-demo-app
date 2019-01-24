914f419c97a332675d133b9490e766367fe799d4
Gabor Orosz <gabor.orosz@appcraft.hu>
2019-01-24 20:57:37 +0100

    Add person model data class

---
+++ app/src/main/java/com/example/demodatingapp/model/PersonModel.kt
@@ -0,0 +1,11 @@
+package com.example.demodatingapp.model
+
+data class PersonModel(
+        val avatar: Int,
+        val name: String,
+        val rating: Int,
+        val job: String,
+        val formattedAge: String,
+        val image: Int,
+        val introduction: String) {
+}
