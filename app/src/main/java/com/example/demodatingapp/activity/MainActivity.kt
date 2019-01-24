c0b7210add010c6b420515418362f649d9a65d28
Gabor Orosz <gabor.orosz@appcraft.hu>
2019-01-24 20:55:47 +0100

    Missing files -> Add app bar to person details screen

--- app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.demodatingapp.activity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.demodatingapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val imageView = findViewById<ImageView>(R.id.person_image)
         imageView.setImageResource(R.drawable.clooney3)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
