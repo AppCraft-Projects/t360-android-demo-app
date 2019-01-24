01aa1d34222b793a87ebba6c2ff9d506743c7569
Gabor Orosz <gabor.orosz@appcraft.hu>
2019-01-24 08:17:38 +0100

    Add image view to person details screen

--- app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.example.demodatingapp
+package com.example.demodatingapp.activity
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.demodatingapp.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.person_image)
+        imageView.setImageResource(R.drawable.clooney3)
     }
 }
