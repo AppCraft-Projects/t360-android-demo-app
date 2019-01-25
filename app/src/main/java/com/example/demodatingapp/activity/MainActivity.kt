de25f005ae3401ef5007989e34b619643db65bb5
Gabor Orosz <gabor.orosz@appcraft.hu>
2019-01-25 16:20:40 +0100

    Add ViewModel to person details screen

--- app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/demodatingapp/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.demodatingapp.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.demodatingapp.R
-import com.example.demodatingapp.model.PersonModel
 import com.example.demodatingapp.view.PersonDetailGalleryView
 import com.example.demodatingapp.view.PersonDetailHeaderView
 import com.example.demodatingapp.view.PersonDetailIntroductionView
+import com.example.demodatingapp.viewmodel.PersonDetailViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +21,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val model = PersonModel.georgeClooney()
-
         headerView = findViewById(R.id.person_detail_header)
-        headerView.bind(model)
-
         gallery = findViewById(R.id.gallery)
-        gallery.bind(model)
-
         introductionView = findViewById(R.id.person_detail_introduction)
-        introductionView.bind(model)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val model = ViewModelProviders.of(this).get(PersonDetailViewModel::class.java)
+        model.getUser().observe(this, Observer {
+            if (it != null) {
+                gallery.bind(it)
+                headerView.bind(it)
+                introductionView.bind(it)
+            }
+        })
     }
 }
